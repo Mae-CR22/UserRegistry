8bf811a00673f2a6962d2e53c305bfb3a1f92c16
Mae-CR22 <agibilibus.mondo@gmail.com>
2024-11-22 12:28:44 +0100

    simplebank 1

---
+++ Simple_bank.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract SimpleBank {
+    //Variales de estado
+    mapping(address => uint256) public balances; //balances de cada usuario
+    address public owner; //direccion del propietario del contrato
+
+//El constructor se ejecuta al desplegar el contrato
+constructor() {
+    owner = msg.sender; //define al propietario del contrato
+}
+  //Funcion para depositar ETH
+
+  function name(params) {
+    code
+  }
+}
