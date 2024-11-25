ca05e0d735fe8a69d7b1e2d40e85cd0389cef73c
Mae-CR22 <agibilibus.mondo@gmail.com>
2024-11-25 19:15:16 +0100

    transferencia ETH

--- Simple_bank.sol
+++ Simple_bank.sol
@@ -20,20 +20,30 @@ _;
   //Funcion para depositar ETH 
 
   function deposit() public payable {
+    require(msg.value > 0,"debes enviar ETH");
     balances[msg.sender] += msg.value; //Incrementa el balance del usuario
 } 
 // Funcion para consultar el balance del usuario
 function getBalance() public view returns (uint256) {
     return balances[msg.sender]; //Devuelve el balance del usuario que llama
 }
-//Funcion para retirar ETH del Balance
+//Funcion para retirar ETH del Balance.
 function withdraw(uint256 _amount) public {
   require(balances[msg.sender] >= _amount, "No tienes sufiectes fondos");
-balances[msg.sender] -= _amount; //Reduce el balance del usuario
-payable(msg.sender).transfer(_amount); //EnVia ETH al usuario
+balances[msg.sender] -= _amount; //Reduce el balance del usuario.
+payable(msg.sender).transfer(_amount); //EnVia ETH al usuario.
 }
 // Funcion para consultar el balance total del contrato (solo propietario).
-function getContracBalance()public view onlyOwner returns (uint256) {
+function getContractBalance()public view onlyOwner returns (uint256) {
     return address(this).balance; //Devuelve el balance total del contrato.
+    }
+
+    //funcion para transferir ETH de un ususario a otro usuario registrado.
+    function transfer(address _to, uint256 _amount) public {
+    require(_to != address(0),"Direccion invalida"); //Asegura que la direccion no sea cero.
+    require(balances[msg.sender] >= _amount,"fondos insuficientes"); //Asegura que el remitente tenga fondos suficientes
+
+balances[msg.sender] -= _amount; // Reduce el balance del remitente.
+balances[_to] += _amount; //suma al balance del destinatario.
     }
 }
